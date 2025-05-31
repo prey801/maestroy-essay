de23351b4e96a848c9417899982d41c8c78bc566
prey801 <musyokibrian047@gmail.com>
2025-05-31 19:25:57 +0300

    small fixes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = 5000;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static('public')); // serve HTML/CSS/JS
+app.use(express.static('public')); 
 
 // Routes
 app.use('/api/auth', authRoutes);
